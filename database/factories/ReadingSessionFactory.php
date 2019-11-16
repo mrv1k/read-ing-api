f49fff30ca6cee2ff14d70117dac4245ed796538
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2019-11-16 18:50:51 -0500

    add reading session factory

---
+++ database/factories/ReadingSessionFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\ReadingSession;
+use Faker\Generator as Faker;
+
+$factory->define(ReadingSession::class, function (Faker $faker) {
+    return [
+        'start' => 1,
+        'end' => 26,
+        'book_id' => factory(App\Models\Book::class),
+    ];
+});
